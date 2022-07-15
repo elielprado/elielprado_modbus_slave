e2434c1244fd61106cd67c34add985cd939bcf2b
elielprado <eliel.resende2010@hotmail.com>
2022-07-15 02:25:33 -0300

    Adicionado o Push Button e o Led.

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 
 // assign the Arduino pin that must be connected to RE-DE RS485 transceiver
 #define TXEN	4 
+const int buttonPin = 7; // the number of the pushbutton pin
+const int ledPin = 13; // the number of the LED pin
 
 // assign pins to KY-15 module
 #define DHTPIN 2
@@ -16,6 +18,22 @@ DHT dht(DHTPIN, DHTTYPE);
 
 // data array for modbus network sharing
 uint16_t data[16];
+
+
+/*
+Data Variable Map:
+Pos (16 bit each) : Meaning
+0 : Temperature (int) (AI)
+1 : Temperature Sensor Status (BOOL) (DO)
+2 : Humidity (int) (AI)
+3 : Humidity Sensor Status (DO) (OUTPUT)
+4 : Push Button (BOOL) (DI) (INPUT)
+5 : LED (BOOL) (DO) (OUTPUT)
+
+
+
+
+*/
 /*uint16_t au16data[16] = {
   3, 1415, 9265, 4, 2, 7182, 28182, 8, 0, 0, 0, 0, 0, 0, 1, -1};*/
 
@@ -36,11 +54,20 @@ void setup() {
   
   slave.start();
   dht.begin();
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+
 }
 
 void loop() {
   unsigned long currentMillis = millis();
-  slave.poll( data, 16 );
+  slave.poll( data, 6 );
+  if(digitalRead(buttonPin)){
+    data[4] = 1;
+  }
+  else{
+    data[4] = 0;
+  }
   if(currentMillis - previousMillis > interval)
   {
   
@@ -52,24 +79,24 @@ void loop() {
   if(myTemp >= 0 && myTemp < 51)
   {
     data[0] = myTemp*100;
-    data[9] = 0; //good data
+    data[1] = 0; //good data
   }else
   {
-    data[9] = 1; //temperature status error
+    data[1] = 1; //temperature status error
   }
   if(myHumidity >= 0 && myHumidity < 101)
   {
-    data[8] = myHumidity;
-    data[10] = 0; //good data
+    data[2] = myHumidity;
+    data[3] = 0; //good data
   }else{
-    data[10] = 1; //humidity status error
+    data[3] = 1; //humidity status error
   }
   
   Serial.print("Temperature: ");
   Serial.println(data[0]);
   Serial.print("Temperature status: ");
   
-  if(data[9] == 1)
+  if(data[1] == 1)
   {
     Serial.println("BAD");
   }
@@ -77,17 +104,26 @@ void loop() {
     Serial.println("GOOD");
   }
   Serial.print("Humidity: ");
-  Serial.println(data[8]);
+  Serial.println(data[2]);
   Serial.print("Humidity status: ");
 
-  if(data[10] == 1)
+  if(data[3] == 1)
   {
     Serial.println("BAD");
   }
   else{
     Serial.println("GOOD");
   }
+
   previousMillis = currentMillis;
+  }
+
+  if(data[5] == 1)
+  {
+    digitalWrite(ledPin, HIGH);
+  }
+  else{
+    digitalWrite(ledPin, LOW);
   }
   //data[0] = myTemp;
   //data[1] = myHumidity;
